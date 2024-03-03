a29fed0946c6ddb1612112f6de6412e9a5a131cb
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-03 11:42:56 +0300

    add shadcn-ui for the home page buttons

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,7 @@
 import Logo from "@/components/logo";
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 
 export default function Page() {
@@ -17,6 +19,18 @@ export default function Page() {
         <Logo/>
         <h1 className="text-5xl font-semibold my-6 max-w-[500px]">Manage your <span className="font-extrabold">pet daycare</span>with ease</h1>
         <p className="text-2xl font-medium max-w-[600px]">Use PetSoft to easily keep track of pets under your care. Get lifetime access for $299.</p>
+        <div className="mt-10">
+          <Button asChild>
+            <Link href="/signup">
+               Get Started
+            </Link>
+            </Button>
+          <Button asChild variant="secondary">
+            <Link href="/signin">
+              Log in
+            </Link>
+            </Button>
+        </div>
       </div>
 
     </main>
