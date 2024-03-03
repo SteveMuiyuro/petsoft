a5910239def9884cdfb340dc123db9c22dc53441
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-03 13:19:36 +0300

    create a reusable H1 component and style the branding and stats components

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -1,7 +1,15 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
 
 
 export default function Page() {
   return (
-    <div>Dashboard Page</div>
+    <main>
+     <div className="flex items-center justify-between text-white py-8  ">
+     <Branding/>
+     <Stats/>
+     </div>
+
+      </main>
   )
 }
