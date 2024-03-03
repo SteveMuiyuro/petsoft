1c760e9732658b1949eee2964e86a3ac807b7e20
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-03 14:20:33 +0300

    style the pet list component

--- components/content-block.tsx
+++ components/content-block.tsx
@@ -4,6 +4,6 @@ export default function ContentBlock({children}:{
     children:React.ReactNode
 }) {
   return (
-    <div className="bg-[#F7F8FA] shadow-sm rounded-md overflow-hidden h-full w-full">content-block</div>
+    <div className="bg-[#F7F8FA] shadow-sm rounded-md overflow-hidden h-full w-full">{children}</div>
   )
 }
--- components/pet-list.tsx
+++ components/pet-list.tsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import Image from 'next/image'
+
 
 export default function PetList() {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
         <li>
-            <button>
-                Benjamin
+            <button className="flex  h-[70px] w-full cursor-pointer items-center text-base px-5 gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+            <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet Image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+
+            />
+                <p className="font-semibold">Benjamin</p>
             </button>
         </li>
     </ul>
