1c15c0cf471d618fe2df5bc1eb52d2efbfb97793
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-03 14:06:29 +0300

    make the dashboard page responsive

--- app/(app)/app/dashboard/page.tsx
+++ app/(app)/app/dashboard/page.tsx
@@ -14,19 +14,19 @@ export default function Page() {
      <Stats/>
      </div>
 
-     <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
+     <div className="grid md:grid-cols-3 md:grid-rows-[45px_1fr] grid-rows-[45px_300px_500px] gap-4 md:h-[600px]">
 
-      <div className="row-start-1 row-span-1 col-start-1 col-span-1">
+      <div className="md:row-start-1 md:row-span-1 md:col-start-1 md:col-span-1">
          <SearchForm/>
       </div>
 
-      <div className="row-start-2 row-span-full col-start-1 col-span-1">
+      <div className="md:row-start-2 md:row-span-full md:col-start-1 md:col-span-1">
       <ContentBlock>
            <PetList/>
       </ContentBlock>
       </div>
 
-      <div className="row-start-1 row-span-full col-start-2  col-span-full">
+      <div className="md:row-start-1 md:row-span-full md:col-start-2  md:col-span-full">
 
       <ContentBlock>
            <PetDetails/>
