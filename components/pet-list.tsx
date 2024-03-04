07718c0b8f6fda07c4e5b0545db772e9bb385bdc
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-04 12:23:27 +0300

    add conditional background color to the button

--- components/pet-list.tsx
+++ components/pet-list.tsx
@@ -2,6 +2,7 @@
 
 import { usePetsContext } from '@/lib/hooks'
 import { Pet } from '@/lib/types'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
 
 type PetListProps = {
@@ -11,14 +12,18 @@ type PetListProps = {
 
 export default function PetList() {
 
-  const {pets, handleActiveId} = usePetsContext()
+  const {pets, handleActiveId, activePetId} = usePetsContext()
 
   return (
     <ul className="bg-white border-b border-black/[0.08]">
 
     {pets.map(pet => (
       <li key ={pet.id}>
-          <button onClick={()=> handleActiveId(pet.id)}className="flex  h-[70px] w-full cursor-pointer items-center text-base px-5 gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <button onClick={()=> handleActiveId(pet.id)}className={cn("flex  h-[70px] w-full cursor-pointer items-center text-base px-5 gap-3 hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition",
+            {
+              "bg-[#EFF1F2]":activePetId === pet.id
+            }
+        )}>
           <Image
           src={pet.imageUrl}
           alt="Pet Image"
