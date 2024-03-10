44381b1ddec565872248e98d773f0e10c7d9dcca
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-10 12:51:32 +0300

    add the placeholder image inside the addpet function

--- lib/actions.ts
+++ lib/actions.ts
@@ -7,7 +7,7 @@ export async function addPet(formData) {
          name: formData.get("name"),
          ownerName: formData.get("ownerName"),
          age: parseInt(formData.get("age")),
-         imageUrl:formData.get("imageUrl"),
+         imageUrl:formData.get("imageUrl") || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
          notes:formData.get("notes")
         },
     })
