d4192adcfa2c77c073e013607ed53e4cb3ee1c11
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-10 12:28:09 +0300

    replace handle sub,it function with the action attribute

--- components/pet-form.tsx
+++ components/pet-form.tsx
@@ -6,6 +6,7 @@ import { Input } from './ui/input'
 import { Textarea } from './ui/textarea'
 import { Button } from './ui/button'
 import { usePetsContext } from '@/lib/hooks'
+import { addPet } from '@/lib/actions'
 
 
 type PetFormProps = {
@@ -19,22 +20,22 @@ export default function PetForm({actionType, onFormSubmission}:PetFormProps) {
 const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    const formData = new FormData(e.currentTarget);
-     const pet = {
-        name:formData.get("name") as string,
-        ownerName: formData.get("ownerName") as string,
-        imageUrl:formData.get("imageurl") as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
-        age: +(formData.get("age") as string),
-        notes:formData.get("notes") as string
-     }
+    // const formData = new FormData(e.currentTarget);
+    //  const pet = {
+    //     name:formData.get("name") as string,
+    //     ownerName: formData.get("ownerName") as string,
+    //     imageUrl:formData.get("imageurl") as string || "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+    //     age: +(formData.get("age") as string),
+    //     notes:formData.get("notes") as string
+    //  }
 
-    actionType === "add" ?  handleAddPet(pet) : handleEditPet(selectedPet!.id, pet)
-    onFormSubmission()
+    // actionType === "add" ?  handleAddPet(pet) : handleEditPet(selectedPet!.id, pet)
+    // onFormSubmission()
 
 
     }
   return (
-    <form onSubmit={handleSubmit}className="flex flex-col ">
+    <form action={addPet}className="flex flex-col ">
         <div className='space-y-3'>
             <div className="space-y-1">
                 <Label htmlFor='name'>Name</Label>
@@ -47,8 +48,8 @@ const handleSubmit = (e:React.FormEvent<HTMLFormElement>) => {
             </div>
 
             <div className="space-y-1">
-                <Label htmlFor='imageurl'>ImageUrl</Label>
-                <Input name="imageurl" id="imageurl" type="text" defaultValue={actionType==="edit" ? selectedPet?.imageUrl: ""}/>
+                <Label htmlFor='imageUrl'>ImageUrl</Label>
+                <Input name="imageUrl" id="imageUrl" type="text" defaultValue={actionType==="edit" ? selectedPet?.imageUrl: ""}/>
             </div>
 
 
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,9 +1,15 @@
 "use server";
 
 import prisma from "./db"
-export async function addPet(pet) {
+export async function addPet(formData) {
    await prisma.pet.create({
-        data:pet,
+        data:{
+         name: formData.get("name"),
+         ownerName: formData.get("ownerName"),
+         age: parseInt(formData.get("age")),
+         imageUrl:formData.get("imageUrl"),
+         notes:formData.get("notes")
+        },
     })
 
 }
