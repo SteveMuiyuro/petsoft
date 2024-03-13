b88c9a205c2bfed83d8cd6ecfedfafed4ce75d0d
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-13 19:27:17 +0300

    add sign in validation logic

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,10 +1,39 @@
 import NextAuth, { NextAuthConfig } from "next-auth"
+import credentials from "next-auth/providers/credentials";
+
 
 const config = {
     pages:{
         signIn:"/signin"
     },
-    providers:[],
+    providers:[
+        credentials({
+         async authorize(credentials) {
+            const {email, password} = credentials;
+
+            const user = prisma?.user.findUnique({
+                where: {
+                    email
+                }
+            })
+
+            if(!user){
+                console.log("user not found")
+                return null;
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.hashedPassword)
+
+            if(!passwordMatch){
+                console.log("Invalid credentials")
+                return null;
+            }
+
+            return user;
+
+         }
+        })
+    ],
     callbacks:{
         authorized:({request}) => {
 
