7ed5ffee9158f658fe4fe69c0f67ea5e62b64801
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-14 12:32:35 +0300

    display user details on account page

--- app/(app)/app/account/page.tsx
+++ app/(app)/app/account/page.tsx
@@ -1,12 +1,20 @@
 import ContentBlock from "@/components/content-block";
 import H1 from "@/components/h1";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+
+  const session = await auth()
+
+  if(!session?.user){
+    redirect("/signin")
+  }
   return (
     <main>
       <H1 className="my-8 text-white">My Account</H1>
       <ContentBlock className="h-[500px] flex items-center justify-center">
-        <p>You are logged in as ....</p>
+        <p>You are logged in as {session.user.email}</p>
       </ContentBlock>
     </main>
   )
