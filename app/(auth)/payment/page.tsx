4ec53ca50e30778e5a954f8dcf8246b6caf2f144
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-18 10:44:14 +0300

    add loading state to the purchase button

--- app/(auth)/payment/page.tsx
+++ app/(auth)/payment/page.tsx
@@ -2,23 +2,29 @@
 import H1 from '@/components/h1'
 import { Button } from '@/components/ui/button'
 import { createCheckoutSession } from '@/lib/actions'
-import React from 'react'
+import React, { useTransition } from 'react'
 
 export default function page({searchParams}) {
+
+  const [isPending, startTransition] = useTransition()
   return (
     <main className="flex flex-col gap-y-10 items-center">
         <H1>Make a one time payment to access petsoft</H1>
 
       {!searchParams.success  &&
-                <Button onClick={async ()=>{
+                <Button disabled={isPending} onClick={async () => {
+                  startTransition(async ()=>{
                     await createCheckoutSession()
-                }
+                })}
+
 
             }>Purchase lifetime access for EURO 299 only</Button>
 
       }
 
       {searchParams.success && <p className="text-green-700 text-md">Payment succesfull, you now have lifetime access</p>}
+
+      {searchParams.cancelled && <p className="text-red-700 text-md">Payment cancelled, try again</p>}
     </main>
   )
 }
