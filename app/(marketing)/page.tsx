a256f31d7916fe92acbcca5b11af3a32b4195c5f
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-19 17:03:28 +0300

    adjust heading

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
 
       <div>
         <Logo/>
-        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">Manage your <span className="font-extrabold">pet daycare</span>with ease</h1>
+        <h1 className="text-5xl font-semibold my-6 max-w-[500px]">Manage your <span className="font-extrabold">pet daycare</span> with ease</h1>
         <p className="text-2xl font-medium max-w-[600px]">Use PetCare to easily keep track of pets under your care. Get lifetime access for €299.</p>
         <div className="mt-10 space-x-5">
           <Button asChild>
--- components/branding.tsx
+++ components/branding.tsx
@@ -4,7 +4,7 @@ import H1 from './h1'
 export default function Branding() {
   return (
     <section>
-        <H1>Pet <span className="font-semibold">Soft</span></H1>
+        <H1>Pet <span className="font-semibold">Care</span></H1>
         <p className="text-lg opacity-80">Manage your pet daycare with ease</p>
       </section>
   )
