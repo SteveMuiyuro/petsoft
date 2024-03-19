b36af23adad1b04436a5c8e1447bc1bdd4f8c7e1
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-19 17:00:01 +0300

    change background color

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <main className="bg-[#5DC9A8] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
+    <main className="bg-[#67da93] min-h-screen flex flex-col xl:flex-row items-center justify-center gap-10">
 
       <Image
         src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
@@ -18,7 +18,7 @@ export default function Page() {
       <div>
         <Logo/>
         <h1 className="text-5xl font-semibold my-6 max-w-[500px]">Manage your <span className="font-extrabold">pet daycare</span>with ease</h1>
-        <p className="text-2xl font-medium max-w-[600px]">Use PetSoft to easily keep track of pets under your care. Get lifetime access for $299.</p>
+        <p className="text-2xl font-medium max-w-[600px]">Use PetCare to easily keep track of pets under your care. Get lifetime access for €299.</p>
         <div className="mt-10 space-x-5">
           <Button asChild>
             <Link href="/signup">
