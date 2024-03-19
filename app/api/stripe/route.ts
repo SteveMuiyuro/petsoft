842276c6c9021c19297961cf4ca925e70b50a778
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-03-19 09:45:09 +0300

    add function to update database

--- app/api/stripe/route.ts
+++ app/api/stripe/route.ts
@@ -1,7 +1,20 @@
+import prisma from "@/lib/db"
+
 export async function POST(request:Request){
 
-    const body = await request.text()
+    const data = await request.json()
 
-    return Response.json(null, {status:200});
+   await prisma?.user.update({
+
+    where:{
+        email:data.data.object.customer_email
+    },
+    data:{
+        hasAccess:true
+    }
+   })
+
+
+    // return Response.json(null, {status:200});
 
 }
